94f10c8ebf2913b453fd82437db2905b22921cf5
tonkatsusand <iongenerator@gmail.com>
2015-05-24 00:14:32 +0900

    Add ShurikenScalerTool

---
+++ ShurikenScalerTool.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumibiYakitori.PlaTools {
+	public class ShurikenScalerTool : Tool {
+		public ShurikenScalerTool() : base() { 
+			this.Text = "Scale";
+			this.Filters.NeedSelection = false;
+		} 
+
+		public override void Run() {
+		}
+	}
+}
+
