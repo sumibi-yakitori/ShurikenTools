34263636c0b883da0549671a75c568d93eb21bd6
tonkatsusand <iongenerator@gmail.com>
2015-05-23 18:48:57 +0900

    First commit.

---
+++ ShurikenTools.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumibiYakitori.PlaTools {
+	public class CreateShurikenTool : Tool {
+		public CreateShurikenTool() : base() { 
+			this.Text = "Shuriken";
+			this.Filters.NeedSelection = false;
+		} 
+
+		public override void Run() {
+			Selection.activeGameObject = new GameObject("Particle System").AddComponent<ParticleSystem>().gameObject;
+			Undo.RegisterCreatedObjectUndo(Selection.activeGameObject, this.GetType().Name);
+		}
+	}
+}
+
